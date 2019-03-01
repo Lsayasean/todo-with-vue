accacd037d2e304bb013cdaf62eac4d736a32f4b
Lanrna Sayasean <lsayasean@gmail.com>
2019-03-01 10:37:43 -0500

    created my first todo app wth vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todo 
+    v-bind:todos='todos'
+    v-on:del-todo="deleteTodo"
+    />
+    <AddToDo 
+    v-on:add-todo='addTodo'
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo'
+import AddToDo from './components/AddToDo'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Todo,
+    AddToDo
+  },
+  data(){
+    return {
+      // this is the state/data
+      todos: [
+        {
+          id: 1,
+          title: 'Code',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Code again',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Get ready for FIRST SOFTWARE JOB',
+          completed: false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+    addTodo(newTodo){
+      this.todos = [...this.todos, newTodo]
+    }
   }
 }
 </script>
